643fbabd477706f3eb223cb772d7f3721c037e87
Prudhvi4311 <prudhviraj37371@gmail.com>
2025-08-04 23:49:05 +0530

    changes

--- tests/pract/loginwithtestdata.spec.js
+++ tests/pract/loginwithtestdata.spec.js
@@ -1,10 +1,17 @@
 const {test,expect} = require("@playwright/test")
 import testdata from "../../testdata.json"
 
-test("login with test data",async({page})=>{
+
+test.describe("check the various data driven test", () =>{
+
+testdata.forEach(data =>{
+    test(`login with test data ${data.testwithdata}`, async ({ page }) => {
+    
     await page.goto("https://freelance-learn-automation.vercel.app/login")
 
-    await page.locator("#email1").fill(testdata.username,{delay:2000})
-    await page.locator("#password1").fill(testdata.password,{delay:2000})
+    await page.locator("#email1").fill(data.username, { delay: 2000 })
+    await page.locator("#password1").fill(data.password, { delay: 2000 })
     await page.locator("[type='submit']").click()
+})
+})
 })
